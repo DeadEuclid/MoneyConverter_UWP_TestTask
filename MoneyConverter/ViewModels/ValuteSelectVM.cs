e4ce3c21f4bf55c80b12a257708d6bcfff5bd9f1
DeadEuclid <60969774+DeadEuclid@users.noreply.github.com>
2021-10-18 07:44:41 +0700

    Не могу нормально связать VM  и View

---
+++ MoneyConverter/ViewModels/ValuteSelectVM.cs
@@ -0,0 +1,14 @@
+﻿using MoneyConverter.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyConverter.ViewModels
+{
+    class ValuteSelectVM:ViewModel
+    {
+
+    }
+}
